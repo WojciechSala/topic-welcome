679c8eabe8d69221fa8bd66865736b95281079f7
WojciechSala <wojteksala190@gmail.com>
2020-04-21 22:22:11 +0200

    Updates

---
+++ precache-manifest.a3619522cc9da37edb3efaf12a7552cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41d931fa8bcba10fb3d13c61b4af8f0",
+    "url": "/topic-welcome/index.html"
+  },
+  {
+    "revision": "f2bf88007179aa46952f",
+    "url": "/topic-welcome/static/css/main.1cbf0987.chunk.css"
+  },
+  {
+    "revision": "1ee13396fdb7295a71ab",
+    "url": "/topic-welcome/static/js/2.42e16d9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/topic-welcome/static/js/2.42e16d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2bf88007179aa46952f",
+    "url": "/topic-welcome/static/js/main.8c8918af.chunk.js"
+  },
+  {
+    "revision": "5669ea7e81b27f2818e2",
+    "url": "/topic-welcome/static/js/runtime-main.f66a7431.js"
+  }
+]);
